69e9683554310c4c41063d594d2163a4621674fb
Arko Kundu <kunduarko@gmail.com>
2021-07-24 13:45:10 -0700

    added load more button

--- movie-app/src/components/Home.js
+++ movie-app/src/components/Home.js
@@ -14,6 +14,7 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 
 //import Hook 
 import {useHomeFetch} from '../hooks/useHomeFetch';
@@ -21,13 +22,15 @@ import {useHomeFetch} from '../hooks/useHomeFetch';
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const {state, loading, error, setSearchTerm} = useHomeFetch();
+    const {state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore} = useHomeFetch();
     
+    if (error) return <div>Something went wrong...</div>;
+
     console.log(state);
 
     return (
         <>
-            {state.results[0] ? (
+            {!searchTerm && state.results[0] ? (
             <HeroImage
                 image = {`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
                 title = {state.results[0].original_title}
@@ -37,7 +40,7 @@ const Home = () => {
             : null
             }
             <SearchBar setSearchTerm={setSearchTerm} />
-            <Grid header='Popular Movies'>
+            <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
                 {state.results.map(movie=>(
                     <Thumb 
                     key={movie.id}
@@ -51,7 +54,10 @@ const Home = () => {
                     />
                 ))}
             </Grid>
-            <Spinner />
+            {loading && <Spinner />}
+            {state.page < state.total_pages && !loading && (
+                <Button text='Load More' callback={()=>setIsLoadingMore(true)} />
+            )}
         </>
     );
 }
